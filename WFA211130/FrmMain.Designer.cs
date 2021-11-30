44b0d2ef4262bcec9c8241b75756debb2962b2de
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2021-11-30 09:47:24 +0100

    Add project files.

---
+++ WFA211130/FrmMain.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WFA211130
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbKereses = new System.Windows.Forms.TextBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cbNem = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "név vagy csapatnév:";
+            // 
+            // tbKereses
+            // 
+            this.tbKereses.Location = new System.Drawing.Point(235, 33);
+            this.tbKereses.Margin = new System.Windows.Forms.Padding(4);
+            this.tbKereses.Name = "tbKereses";
+            this.tbKereses.Size = new System.Drawing.Size(436, 32);
+            this.tbKereses.TabIndex = 1;
+            this.tbKereses.TextChanged += new System.EventHandler(this.TbKereses_TextChanged);
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.AllowUserToResizeColumns = false;
+            this.dgv.AllowUserToResizeRows = false;
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dgv.Location = new System.Drawing.Point(16, 128);
+            this.dgv.Margin = new System.Windows.Forms.Padding(4);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersVisible = false;
+            this.dgv.Size = new System.Drawing.Size(1026, 439);
+            this.dgv.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.FillWeight = 20F;
+            this.Column1.HeaderText = "id";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.FillWeight = 50F;
+            this.Column2.HeaderText = "név";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.FillWeight = 70F;
+            this.Column3.HeaderText = "csapat";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.FillWeight = 40F;
+            this.Column4.HeaderText = "nemzetiség";
+            this.Column4.Name = "Column4";
+            // 
+            // cbNem
+            // 
+            this.cbNem.FormattingEnabled = true;
+            this.cbNem.Location = new System.Drawing.Point(867, 33);
+            this.cbNem.Name = "cbNem";
+            this.cbNem.Size = new System.Drawing.Size(172, 33);
+            this.cbNem.TabIndex = 3;
+            this.cbNem.SelectedIndexChanged += new System.EventHandler(this.cbNem_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(730, 36);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "nemzetiség:";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 581);
+            this.Controls.Add(this.cbNem);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.tbKereses);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "FrmMain";
+            this.Text = "Kerseső proto";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbKereses;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.ComboBox cbNem;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WFA211130/FrmMain.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA211130
+{
+    public partial class FrmMain : Form
+    {
+        public string ConnectionString { private get; set; }
+        public FrmMain()
+        {
+            ConnectionString = 
+                "Server = (localdb)\\MSSQLLocalDB;" +
+                "Database = verseny;";
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            FillDGV();
+            FillCB();
+        }
+
+        private void FillCB()
+        {
+            using (var c = new SqlConnection(ConnectionString))
+            {
+                c.Open();
+                var r = new SqlCommand(
+                    "SELECT DISTINCT nemzetiseg FROM versenyzo;",
+                    c).ExecuteReader();
+                while (r.Read()) cbNem.Items.Add(r[0]);
+            }
+        }
+
+        private void FillDGV()
+        {
+            dgv.Rows.Clear();
+
+            using (var c = new SqlConnection(ConnectionString))
+            {
+                c.Open();
+
+                string whereNemzetiseg = cbNem.SelectedIndex == -1
+                    ? "" : $"AND nemzetiseg = '{cbNem.Text}' ";
+
+                var r = new SqlCommand(
+                    "SELECT versenyzo.id, nev, csapatNev, nemzetiseg " +
+                    "FROM versenyzo " +
+                    "INNER JOIN csapat ON csapatId = csapat.id " +
+                    $"WHERE (nev LIKE '{tbKereses.Text}%' " +
+                        $"OR csapatnev LIKE '{tbKereses.Text}%') " +
+                        whereNemzetiseg +
+                    "ORDER BY nev;", c).ExecuteReader();
+
+                while (r.Read())
+                {
+                    dgv.Rows.Add(r[0], r[1], r[2], r[3]);
+                }
+            }
+        }
+
+        private void TbKereses_TextChanged(object sender, EventArgs e)
+        {
+            FillDGV();
+        }
+
+        private void cbNem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FillDGV();
+        }
+    }
+}
